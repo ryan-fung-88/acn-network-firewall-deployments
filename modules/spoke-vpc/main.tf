295e94c9c9715b982f1302b52315b97c0b9d3061
rkfung <rkfung88@gmail.com>
2025-01-02 22:47:14 -0600

    spoke-vpc code update

--- modules/spoke-vpc/main.tf
+++ modules/spoke-vpc/main.tf
@@ -1,3 +1,10 @@
+locals {
+  public_subnets_count = length(var.public_subnets)
+  private_subnets_count = length(var.private_subnets)
+  create_multiple_public_route_tables = var.multiple_public_route_tables
+  create_multiple_private_route_tables = var.multiple_private_route_tables
+}
+
 resource "aws_vpc" "this_spoke_vpc" {
     cidr_block = var.cidr
     instance_tenancy = var.instance_tenancy
@@ -8,15 +15,53 @@ resource "aws_vpc" "this_spoke_vpc" {
         var.vpc_tags,
     )
 }
+
 resource "aws_subnet" "this_spoke_public_subnets" {
-    count = length(var.public_subnets) > 0 ? len(var.public_subnets) : 0
+    count = local.public_subnets_count > 0 ? local.public_subnets_count : 0
+
     cidr_block = element(var.public_subnets,count.index)
     availability_zone = element(var.avalibility_zones,count.index)
     vpc_id = aws_vpc.this_spoke_vpc.id
+    enable_dns64 = var.enable_public_dns64
+    map_public_ip_on_launch = var.map_public_ip_on_launch
 }
+
 resource "aws_subnet" "this_spoke_private_subnets" {
-    count = length(var.private_subnets) > 0 ? len(var.private_subnets) : 0
+    count = local.private_subnets_count > 0 ? local.private_subnets_count : 0
+
     cidr_block = element(var.private_subnets,count.index)
     availability_zone = element(var.avalibility_zones,count.index)
     vpc_id = aws_vpc.this_spoke_vpc.id
+    enable_dns64 = var.enable_private_dns64
+    private_dns_hostname_type_on_launch = var.private_dns_hostname_type_on_launch
 }
+
+resource "aws_route_table" "public_route_table" {
+    count = local.create_multiple_public_route_tables ? local.public_subnets_count : 1
+    vpc_id = aws_vpc.this_spoke_vpc.id
+}
+
+resource "aws_route_table" "private_route_table" {
+    count = local.create_multiple_private_route_tables ? local.private_subnets_count : 1
+    vpc_id = aws_vpc.this_spoke_vpc.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = local.create_multiple_public_route_tables ? local.public_subnets_count : 1
+  subnet_id = element(var.public_subnets,count.index)
+  route_table_id = element(aws_route_table.public_route_table[*].id,count.index)
+}
+
+resource "aws_route_table_association" "private" {
+  count = local.create_multiple_private_route_tables ? local.private_subnets_count : 1
+  subnet_id = element(var.private_subnets,count.index)
+  route_table_id = element(aws_route_table.private_route_table[*].id,count.index)
+
+}
+
+# resource "aws_route" "spoke_vpc_to_tgw" {
+#   count = local.create_multiple_public_route_tables ? local.public_subnets_count : 1
+#   route_table_id = element(aws_route_table.public_route_table[*].id,count.index)
+#   destination_cidr_block = var.destination_cidr_block
+#   transit_gateway_id = var.transit_gateway_id
+# }
